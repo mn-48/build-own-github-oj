2200dbe6d9d36bc50721de7c3c8d8876340065ae
mn-48 <nazmul.cse48@gmail.com>
2025-02-06 10:18:08 +0600

    test case modifide

--- test.py
+++ test.py
@@ -1,9 +1,10 @@
 # test.py
 def test_case():
     # Example test case
-    result = 1 + 2
+    result = 5 + 2
     expected = 2
     return result == expected
+
 
 if __name__ == "__main__":
     if test_case():
